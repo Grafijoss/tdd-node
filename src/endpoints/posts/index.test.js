e094622706501b8b0e3b06f6bdf1acdb888c5e14
grafijoss <grafijoss@hotmail.com>
2021-02-05 00:19:59 -0500

    segundo test de integracion FAIL POST

--- src/endpoints/posts/index.test.js
+++ src/endpoints/posts/index.test.js
@@ -20,6 +20,20 @@ describe("Server", () => {
         expect(response.body.userId).toEqual(5);
         expect(response.body).toHaveProperty("id");
       });
+      it("does not creates a new post", async () => {
+        // le pasamos la aplicación de express la cual queremos testear
+        // la respuesta que va a devolver app
+        const response = await request(app)
+          // vamos a hacer un post
+          .post("/")
+          // le indicamos que datos vamos a enviar
+          .send({ userId: 100 })
+          // decoradores, cabeceras o headers
+          .set("user_id", 1)
+          .set("Content-Type", "application/json");
+        // console.log(response);
+        expect(response.statusCode).toEqual(400);
+      });
     });
   });
 });
